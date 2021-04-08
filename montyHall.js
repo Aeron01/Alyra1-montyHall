b715bef95fb5598b1209823d781031c7b7dabcfe
Aeron01 <one.soulreaper@gmail.com>
2021-04-08 18:43:21 +0200

    Not ready but it working !

--- montyHall.js
+++ montyHall.js
@@ -10,7 +10,10 @@ const portes = ['Chèvre', 'Chèvre', 'Chèvre']
 
 let Gates = portes
 let fstChoice = 4
-let changeChoice = 1
+let changeChoice = false
+let GoatDem = 0
+let lstGates = 0
+let lstChoice = 0
 
 const index = randomInt(0, 3) // un nombre aléatoire entre 0 et 2
 
@@ -20,48 +23,56 @@ let runGame = true
 let indexPlayer = 0
 
 while (runGame) {
-  const start = () => {
-    console.log(Gates)
-    const choicePlayer = readlineSync.keyIn(`Trois portes s'offre à vous. Laquelle choisissez-vous (1, 2 ou 3 - 0 pour sortir du jeux) ? : `, { limit: ['$<1-3>', '0'] });
-    indexPlayer = choicePlayer - 1
-    console.log(indexPlayer)
-    if (choicePlayer === '0') {
-      console.log('GAME OVER')
-      process.exit(1)
-    }
 
-    if (changeChoice === 1) {
-      for (let i = 0; i < Gates.length; i++) {
-        if (i !== indexPlayer && i !== index) {
-          console.log(`Je vous dévoile la porte ${i + 1}\n et voici ce qu'il y a derrière : une ${Gates[i]}`)
-          break
-        }
-      }
+  console.log(Gates)
+  const choicePlayer = readlineSync.keyIn(`Trois portes s'offre à vous. Laquelle choisissez-vous (1, 2 ou 3 - 0 pour sortir du jeux) ? : `, { limit: ['$<1-3>', '0'] });
+  indexPlayer = choicePlayer - 1
+  console.log(`index 1er choix joueur ${indexPlayer}`)
+  if (choicePlayer === '0') {
+    console.log('GAME OVER')
+    process.exit(1)
+  }
+
+
+  for (let i = 0; i < Gates.length; i++) {
+    if (i !== indexPlayer && i !== index) {
+      console.log(`Je vous dévoile la porte ${i + 1}\n et voici ce qu'il y a derrière : une ${Gates[i]}`)
+      GoatDem = i
+      break
     }
   }
-  start()
+  console.log(`porte chevre dévoiler ${GoatDem}`)
 
-  if (changeChoice === 1) {
-    if (readlineSync.keyInYN('Voulez-vous changer de porte ?')) {
-      fstChoice = indexPlayer
-      changeChoice--
-      start()
-    } else {
-      console.log(`Bien, votre choix sur la porte ${indexPlayer} est vérouiller. Voici ce qui se cache derrière la porte.`)
+  for (let i = 0; i < Gates.length; i++) {
+    if (i !== indexPlayer && i !== GoatDem) {
+      lstGates = i
     }
-    console.log(`Votre choix de la porte ${indexPlayer} est vérouiller. Voici ce qui se cache derrière la porte.`)
   }
-  console.log(indexPlayer)
-  if (changeChoice === 1) {
+
+  console.log(`derniere porte pas devoiler ${lstGates}`)
+
+  if (readlineSync.keyInYN('Voulez-vous changer de porte ?')) {
+    fstChoice = indexPlayer
+    lstChoice = lstGates
+    changeChoice = true
+    console.log(`Votre choix de la porte ${lstChoice} est vérouiller. Voici ce qui se cache derrière la porte.`)
+  } else {
+    console.log(`Bien, votre choix sur la porte ${indexPlayer} est vérouiller. Voici ce qui se cache derrière la porte.`)
+  }
+
+  console.log(`index 1er choix joueur ${indexPlayer}`)
+  console.log(`index 2eme choix joueur ${lstChoice}`)
+
+  if (changeChoice === false) {
     if (indexPlayer === index) {
       console.log(`Vous avez gagner une superbe ${trophy} toutes options, d'une valeur de 181 152€. Félicitation !`)
     } else {
       console.log(`MHEEEEEEE !!!!\nOh, quelle dommage ! Une chèvre !`)
-      console.log(`Vous avez perdu, malheureusement.\n La ${trophy} étais derrière la porte ${index}.`)
+      console.log(`Vous avez perdu, malheureusement.\n La ${trophy} étais derrière la porte ${index + 1}.`)
       console.log(`Mais vous ne repartez pas les mains vide, voici votre lot de consolation.`)
       console.log()
     }
-  } else if (indexPlayer === index) {
+  } else if (lstChoice === index) {
     console.log(`Vous avez gagner une superbe ${trophy} toutes options, d'une valeur de 181 152€. Félicitation !`)
   } else {
     console.log(`MHEEEEEEE !!!!\nOh, quelle dommage ! Une chèvre !`)
@@ -80,7 +91,7 @@ while (runGame) {
     }
   }
   if (readlineSync.keyInYN('Voulez-vous rejouer ?')) {
-    start()
+
   } else {
     console.log('FIN DE PARTIE')
     runGame = false
